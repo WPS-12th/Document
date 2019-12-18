759c880dfb9964ec6503dc260de6606057b6c3ce
이한영 <dev@lhy.kr>
2019-12-18 17:46:37 +0900

    choices주석 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # python manage.py shell_plus실행 위해 필요
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
